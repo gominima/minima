2135c896043f2e723979a5075650182840b54260
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-10-30 12:58:36 +0530

    Added 🦄(workflows/start)

---
+++ fiable.go
@@ -0,0 +1,39 @@
+package fiable
+
+import (
+	
+	"fmt"
+	
+	"net/http"
+	
+	"time"
+)
+
+
+      type fiable struct {
+	server       *http.Server
+	started      bool
+	Timeout      time.Duration
+	properties   map[string]interface{}
+      }
+      
+      func New() *fiable { 
+	  var fiable *fiable = &fiable{}    
+	  return fiable
+      }
+
+      func (f*fiable) Listen(addr string) error {
+	server := &http.Server{Addr: addr}
+	 if f.started {
+		fmt.Errorf("Server is already running", f)
+	 }
+	 f.server = server
+	 f.started = true
+	 return f.server.ListenAndServe()
+      }
+
+
+
+
+
+      
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gofiable/fiable
+
+go 1.17
