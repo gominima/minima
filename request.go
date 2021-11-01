3eebc34de4e23bdf1c362e42900a9603c7cf5a8e
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-11-01 12:03:49 +0530

    Add🦄(response struct)

--- request.go
+++ request.go
@@ -1,1 +1,32 @@
 package fiable
+
+import (
+	"encoding/json"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+)
+
+type request struct {
+	ref        *http.Request
+	fileReader *multipart.Reader
+	query      map[string][]string
+	body       map[string][]string
+	method     string
+	url        string
+	_url       *url.URL
+	
+	query      map[string][]string
+	body       map[string][]string
+
+	json       *json.Decoder
+	props      *map[string]interface{}
+}
+
+func Request(httRequest *http.Request, props *map[string]interface{}) *request{
+ req := &request{}
+ req.ref = httRequest
+ req.fileReader = nil
+ req.method = 
+
+}
--- response.go
+++ response.go
@@ -1,1 +1,32 @@
 package fiable
+
+import (
+	"bufio"
+	"net"
+	"net/http"
+)
+
+type response struct {
+	response http.ResponseWriter
+	write *bufio.Writer
+	connection net.Conn
+	url string
+	method string
+	ended bool
+	props *map[string]interface{}
+	host string
+
+}
+
+func Response(rs http.ResponseWriter, req *http.Request, write *bufio.Writer, connection net.Conn, props *map[string]interface{}) *response {
+ res := &response{}
+ res.response = rs
+ res.connection = connection
+ res.url = req.URL.Path
+ res.method = req.Method
+ res.host = req.Host
+ res.write = write
+ res.props = props
+ return res
+
+}
