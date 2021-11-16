314cb41aa6818f3b7f38dedf73d72bc490344c7a
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-11-16 10:51:06 +0530

    Add🦄(ServeHTTP)base func

--- fiable.go
+++ fiable.go
@@ -1,43 +1,65 @@
 package fiable
 
 import (
+	"context"
 	"fmt"
+	"strings"
 
 	"net/http"
 	"time"
 )
 
-      type fiable struct {
-	server       *http.Server
-	started      bool
-	Timeout      time.Duration
-	router        *router
-	
-	properties   map[string]interface{}
-      }
-      
-      func New() *fiable { 
-	  var router *router = Router()
-	  var fiable *fiable = &fiable{router: router,}    
-	  return fiable
-      }
+type fiable struct {
+	server  *http.Server
+	started bool
+	Timeout time.Duration
+	router  *router
 
+	properties map[string]interface{}
+}
 
-     
+type ctxKey struct{}
 
-      func (f*fiable) Listen(addr string) error {
+func New() *fiable {
+	var router *router = Router()
+	var fiable *fiable = &fiable{router: router}
+	return fiable
+}
+
+func (f *fiable) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var allow []string
+	for _, route := range f.router.routes {
+		matches := route.regex.FindStringSubmatch(r.URL.Path)
+		if len(matches) > 0 {
+			if r.Method != route.method {
+				allow = append(allow, route.method)
+				continue
+			}
+			ctx := context.WithValue(r.Context(), ctxKey{}, matches[1:])
+			route.handler(w, r.WithContext(ctx))
+			return
+		}
+	}
+	if len(allow) > 0 {
+		w.Header().Set("Allow", strings.Join(allow, ", "))
+		http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.NotFound(w, r)
+	fmt.Fprint(w, "Uh")
+}
+
+func (f *fiable) Listen(addr string) error {
 	server := &http.Server{Addr: addr}
-	 if f.started {
+	if f.started {
 		fmt.Errorf("Server is already running", f)
-	 }
-	 f.server = server
-	 f.started = true
-	 return f.server.ListenAndServe()
-      }
-       
+	}
+	f.server = server
+	f.started = true
 
-  
+	return f.server.ListenAndServe()
+}
 
-
-      
- 
+func (f *fiable) Get(path string, handler http.HandlerFunc) {
+	f.router.Get(path, handler)
+}
--- router.go
+++ router.go
@@ -10,3 +10,24 @@ type route struct {
 	regex   *regexp.Regexp
 	handler http.HandlerFunc
 }
+
+type router struct {
+	routes []route
+}
+
+func Router() *router {
+	return &router{}
+}
+
+func (r *router) newRoute(method, pattern string, handler http.HandlerFunc) route {
+	route := route{}
+	route.handler = handler
+	route.regex = regexp.MustCompile("^" + pattern + "$")
+	route.method = method
+	r.routes = append(r.routes, route)
+	return route
+}
+
+func (r *router) Get(pattern string, handler http.HandlerFunc) {
+	r.newRoute("GET", pattern, handler)
+}
