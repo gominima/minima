cbff1cd7c2f2f343d1d9bfb1bdd9d4d3154dce64
shubhaankar <66492212+Shubhaankar-Sharma@users.noreply.github.com>
2022-01-23 11:35:48 +0530

    written basic tests

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/gominima/minima
 
 go 1.17
 
-require github.com/gominima/docgen v0.0.0-20220119070321-3c2d34b92880 // indirect
+require github.com/stretchr/testify v1.7.0
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
--- minima_test.go
+++ minima_test.go
@@ -1,1 +1,46 @@
 package minima
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testServer *httptest.Server
+
+func setupMinima() *minima {
+	app := New()
+	app.Get("/", func(res *Response, req *Request) {
+		res.Status(200).Send("Hello World")
+	})
+	return app
+}
+
+func setup() {
+	minimaApp := setupMinima()
+	testServer = httptest.NewServer(minimaApp)
+}
+
+func shutdown() {
+	testServer.Close()
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	shutdown()
+	os.Exit(code)
+}
+
+func TestHelloWorld(t *testing.T) {
+	resp, err := http.Get(testServer.URL)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	assert.Equal(t, "Hello World", string(body))
+}
