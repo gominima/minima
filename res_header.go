fe29ddf12b8e4cdae70690f8b881649d4a930d79
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-12 09:51:34 +0530

    chore: documentation typo

--- res_header.go
+++ res_header.go
@@ -67,10 +67,9 @@ func (h *OutgoingHeader) Set(key string, value string) *OutgoingHeader {
 }
 
 /**
-@info Sets new header to response
-@param {string} [key] Key of the new header
-@param {string} [value] Value of the new header
-@returns {OutgoingHeader}
+@info Gets the header from response headers
+@param {string} [key] Key of the header
+@returns {string}
 */
 func (h *OutgoingHeader) Get(key string) string {
 	return h.res.Header().Get(key)
