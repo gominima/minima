398dea1f91499e69ab210b2f13f54a584d1cb114
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-13 13:59:21 +0530

    chore: remove useless code

--- mux.go
+++ mux.go
@@ -1,42 +1,9 @@
 package minima
 
 import (
-	"regexp"
 	"strings"
 )
 
-/**
-@info The mux structure
-@property {string} [Path] The path to match
-@property {[]string} [Params] The params to use
-@property {regexp.Regexp} [Regex] The regex to use
-@property {[]Handler} [Handlers] The handlers to use
-*/
-type mux struct {
-	Path     string
-	Params   []string
-	Regex    *regexp.Regexp
-	Handlers []Handler
-}
-
-/**
-@info Match a path
-@param {string} [path] The path to match
-@returns {bool, map[]string[]string}
-*/
-func (m *mux) matchingPath(path string) (bool, map[string]string) {
-	routeParams := make(map[string]string)
-	matchingRegex := m.Regex.FindAllStringSubmatch(path, -1)
-	isMatch := len(matchingRegex) != 0
-
-	if isMatch {
-		for i, param := range m.Params {
-			routeParams[param] = matchingRegex[0][i+1]
-		}
-	}
-
-	return isMatch, routeParams
-}
 
 type Route struct {
 	prefix    string
