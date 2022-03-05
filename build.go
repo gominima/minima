bc3ac846cbef457295963926760879330579d069
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-03-05 12:07:58 +0530

    chore: fix stupid bug

--- build.go
+++ build.go
@@ -17,6 +17,7 @@ func buildHandler(h Handler, params map[string]string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		resp := response(w, req)
 		reqs := request(req)
+		reqs.Params = params
 		h(resp, reqs)
 
 	})
