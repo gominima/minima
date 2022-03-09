8f0e9c15d99f03710c7a258deefbaa4b19b75105
apoorvcodes <apoorvcodes381@gmail.com>
2022-03-09 14:41:56 +0530

    chore: build file docs

--- build.go
+++ build.go
@@ -2,6 +2,13 @@ package minima
 
 import "net/http"
 
+
+/**
+ * @info Converts minima handler into middleware chain handler
+ * @param {Handler} [h] The handler to convert
+ * @param {map[string]string} [params] The handler params
+ * @return {func(http.Handler) http.Handler}
+ */
 func build(h Handler, params map[string]string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
@@ -13,6 +20,12 @@ func build(h Handler, params map[string]string) func(http.Handler) http.Handler 
 	}
 }
 
+/**
+ * @info Converts minima handler into net/http handler func
+ * @param {Handler} [h] The handler to convert
+ * @param {map[string]string} [params] The handler params
+ * @return {http.Handler}
+ */
 func buildHandler(h Handler, params map[string]string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		resp := response(w, req)
